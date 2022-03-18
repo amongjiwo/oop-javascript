582fb304b194f3676e42f23e37307ae214dd80e9
amongjiwo <dahlanakhmad@gmail.com>
2022-03-18 21:11:02 +0700

    Create function volume kubus

---
+++ main.js
@@ -0,0 +1,7 @@
+function volumeKubus(sisi) {
+    const hasil = sisi ** 3;
+    return hasil;
+}
+
+const hasilVolume = volumeKubus(8);
+console.log(`Jika sisi kubus berukuran 8, maka volumenya adalah ${hasilVolume}`);
